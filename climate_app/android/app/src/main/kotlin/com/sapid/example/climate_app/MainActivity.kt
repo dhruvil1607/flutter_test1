37a0c531d385090966a564db120d21e38aedf369
dhruvil1607 <dhruvilprajapati000@gmail.com>
2022-05-05 13:06:15 +0530

    remove files

--- climate_app/android/app/src/main/kotlin/com/sapid/example/climate_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sapid.example.climate_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- data_retrive/android/app/src/main/kotlin/com/example/data_retrive/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.data_retrive
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
